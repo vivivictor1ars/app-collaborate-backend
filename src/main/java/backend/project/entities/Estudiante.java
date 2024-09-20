782e90bcafc08681df0c8c42acb20e606bcdbe42
Vivi <lolxdd1232@hotmail.com>
2024-09-19 21:26:52 -0500

    Entity Estudiante created

---
+++ src/main/java/backend/project/entities/Estudiante.java
@@ -0,0 +1,9 @@
+package backend.project.entities;
+
+public class Estudiante {
+    private Long id;
+    private String semestre_estudiante;
+    private Integer edad_estudiante;
+    private String genero;
+    private String descripcion;
+}

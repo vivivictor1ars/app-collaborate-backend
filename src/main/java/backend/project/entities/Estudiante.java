33bdb6f5dfac350a30a8a8e29b89709fffd0ee91
Vivi <lolxdd1232@hotmail.com>
2024-09-20 00:34:02 -0500

    Entity Estudiante.java modified

--- src/main/java/backend/project/entities/Estudiante.java
+++ src/main/java/backend/project/entities/Estudiante.java
@@ -1,5 +1,12 @@
 package backend.project.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Estudiante {
     private Long id;
     private String semestre_student;

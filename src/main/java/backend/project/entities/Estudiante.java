71f226b9c50979ec848310c1cb0131eaa72f0c84
Vivi <lolxdd1232@hotmail.com>
2024-09-20 00:38:44 -0500

    Entity Estudiante.java modified

--- src/main/java/backend/project/entities/Estudiante.java
+++ src/main/java/backend/project/entities/Estudiante.java
@@ -1,5 +1,9 @@
 package backend.project.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,10 +11,20 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name="estudiantes")
 public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String semestre_student;
     private Integer edad_student;
     private String genero;
     private String descripcion;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name="user_id")
+    private User user;
 }

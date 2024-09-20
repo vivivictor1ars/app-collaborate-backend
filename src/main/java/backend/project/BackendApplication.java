56e5bff1c1be7e1363709d9c38bd3929e4ba4ce3
Vivi <lolxdd1232@hotmail.com>
2024-09-20 15:19:42 -0500

    BackendApplication.java modified to SpringBootApplication

--- src/main/java/backend/project/BackendApplication.java
+++ src/main/java/backend/project/BackendApplication.java
@@ -1,7 +1,9 @@
 package backend.project;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {

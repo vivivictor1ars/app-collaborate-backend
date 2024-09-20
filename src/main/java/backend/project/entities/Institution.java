5b6c6cd359ab2b8bbf47524ebf97f6c8f358c9f5
Vivi <lolxdd1232@hotmail.com>
2024-09-20 15:53:46 -0500

    Entity Institution.java modified

--- src/main/java/backend/project/entities/Institution.java
+++ src/main/java/backend/project/entities/Institution.java
@@ -1,7 +1,20 @@
 package backend.project.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="institution")
 public class Institution {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String correo;
 }

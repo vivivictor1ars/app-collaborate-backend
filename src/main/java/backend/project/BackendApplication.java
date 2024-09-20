914f53acb8d28234816073ec02b3871ddc2895f8
Vivi <lolxdd1232@hotmail.com>
2024-09-19 23:55:49 -0500

    BackendApplication.java modified

--- src/main/java/backend/project/BackendApplication.java
+++ src/main/java/backend/project/BackendApplication.java
@@ -1,22 +1,7 @@
 package backend.project;
 
-import backend.project.entities.Brand;
-import backend.project.entities.Car;
-import backend.project.entities.Model;
-import backend.project.repositories.BrandRepository;
-import backend.project.repositories.CarRepository;
-import backend.project.repositories.ModelRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-
-@SpringBootApplication
-@EnableJpaRepositories("backend.project.repositories")
-@EntityScan("backend.project.entities")
 public class BackendApplication {
 
 	public static void main(String[] args) {

c44a647d53477a763ee4ea0c41674a58613ca94f
Vivi <lolxdd1232@hotmail.com>
2024-09-20 00:57:48 -0500

    Entity Habilidades.java created

---
+++ src/main/java/backend/project/entities/Habilidades.java
@@ -0,0 +1,19 @@
+package backend.project.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="habilidad")
+public class Habilidades {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+}

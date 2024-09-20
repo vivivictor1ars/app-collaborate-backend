14814339f3f2aa3c4ecc09d8a7a25c3ca382d0ce
Vivi <lolxdd1232@hotmail.com>
2024-09-20 01:06:30 -0500

    Entity Habilidad.java modified

--- src/main/java/backend/project/entities/Habilidad.java
+++ src/main/java/backend/project/entities/Habilidad.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Table(name="habilidad")
-public class Habilidades {
+public class Habilidad {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;

6eae8d437eac1c584c1d4618132ee7acef92d941
Vivi <lolxdd1232@hotmail.com>
2024-09-20 00:44:57 -0500

    Entity Carrera_Estudiante.java modified

--- src/main/java/backend/project/entities/Carrera_Estudiante.java
+++ src/main/java/backend/project/entities/Carrera_Estudiante.java
@@ -1,5 +1,28 @@
 package backend.project.entities;
 
-public class Carrera_Estudiante {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class Carrera_Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name="estudiante_id")
+    private Estudiante estudiante;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name="carrera_id")
+    private Carrera carrera;
 }

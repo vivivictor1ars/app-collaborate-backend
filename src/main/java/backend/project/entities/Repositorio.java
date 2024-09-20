e33793121ddba76bcde9685afe653e934aded25f
Vivi <lolxdd1232@hotmail.com>
2024-09-20 00:34:16 -0500

    Entity Repositorio.java modified

--- src/main/java/backend/project/entities/Repositorio.java
+++ src/main/java/backend/project/entities/Repositorio.java
@@ -1,5 +1,12 @@
 package backend.project.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Repositorio {
     private Long id;
     private String nombre_repository;

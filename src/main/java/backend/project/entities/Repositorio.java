b5be3ea7de5f8bdc4bf950bb5afbe864373e07c7
Vivi <lolxdd1232@hotmail.com>
2024-09-20 00:23:08 -0500

    Entity Repositorio.java created

---
+++ src/main/java/backend/project/entities/Repositorio.java
@@ -0,0 +1,10 @@
+package backend.project.entities;
+
+public class Repositorio {
+    private Long id;
+    private String nombre_repository;
+    private String descripcion_repository;
+    private String enlace_repository;
+    private String fecha_repository;
+    private Estudiante estudiante;
+}

cbff3f98ac795037fef5f84aa8125488f27546de
ALDAIR2611 <manuelpuma261@gmail.com>
2024-09-21 10:51:27 -0500

    Entity Institucion.java modified

--- src/main/java/backend/project/entities/Institucion.java
+++ src/main/java/backend/project/entities/Institucion.java
@@ -17,4 +17,33 @@ public class Institucion {
     public Institucion(){
 
     }
+    public Institucion(int id, String nombre_Institucion, String correo_Institucion){
+        this.id = id;
+        this.nombre_Institucion = nombre_Institucion;
+        this.correo_Institucion = correo_Institucion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre_Institucion() {
+        return nombre_Institucion;
+    }
+
+    public void setNombre_Institucion(String nombre_Institucion) {
+        this.nombre_Institucion = nombre_Institucion;
+    }
+
+    public String getCorreo_Institucion() {
+        return correo_Institucion;
+    }
+
+    public void setCorreo_Institucion(String correo_Institucion) {
+        this.correo_Institucion = correo_Institucion;
+    }
 }

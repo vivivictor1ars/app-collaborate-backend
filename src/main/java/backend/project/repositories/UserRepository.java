b47bd3cffe9773e836c52233afdca7e137d2ef76
Vivi <lolxdd1232@hotmail.com>
2024-09-25 20:19:07 -0500

    Repository UserRepository.java was modified

--- src/main/java/backend/project/repositories/UserRepository.java
+++ src/main/java/backend/project/repositories/UserRepository.java
@@ -1,7 +1,32 @@
 package backend.project.repositories;
 
-import backend.project.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Repository;
+import backend.project.entities.User;
+
+import java.util.List;
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByUsername(String username);
+
+    //BUSCAR POR NOMBRE
+    @Query("select count(u.username) from User u where u.username =:username")
+    public int buscarUsername(@Param("username") String nombre);
+
+    //INSERTAR ROLES
+    @Transactional
+    @Modifying
+    @Query(value = "insert into roles (rol, usuario_id) VALUES (:rol, :user_id)", nativeQuery = true)
+    public void insRol(@Param("rol") String authority, @Param("user_id") Long user_id);
+
+    @Query(value = "select count(u.id_usuario) as CantidadUsuarios,u.rol as Rol\n" +
+            "from usuarios u\n" +
+            "group by u.rol;", nativeQuery = true)
+    List<String[]> usuariosporrol();
 }

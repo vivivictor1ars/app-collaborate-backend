ca57b3e13724e552c5778d1f8aecbd2d310ce946
Vivi <lolxdd1232@hotmail.com>
2024-09-25 20:55:16 -0500

    Maven Dependency modifies

---
+++ src/main/java/backend/project/controllers/CarreraController.java
@@ -0,0 +1,50 @@
+package backend.project.controllers;
+
+import backend.project.entities.Carrera;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CarreraController {
+    @Autowired
+    private ICarreraService aS;
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public void insert(@RequestBody CarreraDTO dto){
+        ModelMapper m = new ModelMapper();
+        Carrera e = m.map(dto,Carrera.class);
+        aS.insert(e);
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('ESTUDIANTE')")
+    public List<CarreraDTO> list(){
+        return aS.list().stream().map(x->{
+            ModelMapper m = new ModelMapper();
+            return m.map(x, CarreraDTO.class);
+        }).collect(Collectors.toList());
+    }
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public void delete(@PathVariable("id")Integer id){
+        aS.delete(id);
+    }
+    @GetMapping("/{id}")
+    public CarreraDTO ListId(@PathVariable("id")Integer id){
+        ModelMapper m = new ModelMapper();
+        CarreraDTO dto = m.map(aS.ListId(id), CarreraDTO.class);
+        return dto;
+    }
+    @PutMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public void goUpdate(@RequestBody CarreraDTO dto){
+        ModelMapper m = new ModelMapper();
+        Carrera e = m.map(dto, Carrera.class);
+        aS.insert(e);
+    }
+}

1c204ad5462f8116772de816cc6ab83952ef0eba
Vivi <lolxdd1232@hotmail.com>
2024-09-27 18:14:40 -0500

    Entity User was modified

--- src/main/java/backend/project/entities/User.java
+++ src/main/java/backend/project/entities/User.java
@@ -1,101 +1,12 @@
 package backend.project.entities;
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
-import java.util.List;
-
-@Entity
-@Table(name="users")
-public class User {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idUser;
-    @Column(name="username", length = 60, nullable = false, unique = true)
-    private String username;
-    @Column(name="nombre_User",length = 60,nullable = false)
-    private String nombre_User;
-    @Column(name="contrasena_User",length = 60,nullable = false)
-    private String contrasena_User;
-    private Boolean enabled;
-
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    @JsonIgnore
-    @JoinColumn(name = "user_id")
-    private List<Role> roles;
-
-    @Column(name="rol",length = 60,nullable = false)
-    private String rol;
-
-    public User() {}
-
-    public User(Long idUser, String username, String nombre_User, String contrasena_User, Boolean enabled, List<Role> roles, String rol) {
-        this.idUser = idUser;
-        this.username = username;
-        this.nombre_User = nombre_User;
-        this.contrasena_User = contrasena_User;
-        this.enabled = enabled;
-        this.roles = roles;
-        this.rol = rol;
-    }
-
-    public Long getIdUser() {
-        return idUser;
-    }
-
-    public void setIdUser(Long idUser) {
-
-        this.idUser = idUser;
-    }
-
-    public String getUsername(){
-        return username;
-    }
-    public void setUsername(String username) {
-
-        this.username = username;
-    }
-
-    public String getNombre_User(){
-        return nombre_User;
-    }
-
-    public Boolean getEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(Boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public List<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(List<Role> roles) {
-        this.roles = roles;
-    }
-
-    public String getRol() {
-        return rol;
-    }
-
-    public void setRol(String rol) {
-        this.rol = rol;
-    }
-
-}
-//ROL REEMPLAZA POR AUTHORITY
-//cambiamos usuario por user - USER es paraa nuestro trabajo
-
-/*
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Table(name="usuarios")
-public class Usuario {
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -120,10 +31,10 @@ public class Usuario {
 
     @Column(name="rol",length = 60,nullable = false)
     private String rol;
-    public Usuario() {
+    public User() {
     }
 
-    public Usuario(Long idUsuario, int dni_Usuario, String username, String nombre_Usuario, String correo_Usuario, String contrasena_Usuario, Boolean enabled, List<Role> roles, String rol) {
+    public User(Long idUsuario, int dni_Usuario, String username, String nombre_Usuario, String correo_Usuario, String contrasena_Usuario, Boolean enabled, List<Role> roles, String rol) {
         this.idUsuario = idUsuario;
         this.dni_Usuario = dni_Usuario;
         this.username = username;
@@ -207,7 +118,6 @@ public class Usuario {
         this.rol = rol;
     }
 }
- */
 
 
 

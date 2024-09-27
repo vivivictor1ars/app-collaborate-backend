81c860cd2de0652d63e42f81326903e90af71ea5
Vivi <lolxdd1232@hotmail.com>
2024-09-27 18:33:53 -0500

    SwaggerConfig.java was modified

--- src/main/java/backend/project/config/SwaggerConfig.java
+++ src/main/java/backend/project/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket apiDocket(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("pe.edu.upc.connection2connection.controllers"))
+                .apis(RequestHandlerSelectors.basePackage("backend.project.controllers"))
                 .paths(PathSelectors.any())
                 .build();
     }

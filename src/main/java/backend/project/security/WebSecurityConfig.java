4cc2d6ce8c60aa2d10af61976c8272c99902fc09
Vivi <lolxdd1232@hotmail.com>
2024-09-27 16:19:03 -0500

    Security Component was modified

--- src/main/java/backend/project/security/WebSecurityConfig.java
+++ src/main/java/backend/project/security/WebSecurityConfig.java
@@ -1,4 +1,86 @@
 package backend.project.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Autowired
+    private UserDetailsService jwtUserDetailsService;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Autowired
+    @Qualifier("handlerExceptionResolver")
+    private HandlerExceptionResolver exceptionResolver;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/usuarios").permitAll()
+                .antMatchers(HttpMethod.GET,"/usuarios").permitAll()
+                .antMatchers(HttpMethod.POST,"/roles").permitAll()
+                .antMatchers(HttpMethod.GET,"/roles").permitAll()
+                .antMatchers(HttpMethod.POST,"/{username}").permitAll()
+                .antMatchers(HttpMethod.GET,"/usuarios/username/{username}").permitAll()
+                .antMatchers(HttpMethod.POST,"/estudiantes").permitAll()
+                .antMatchers(HttpMethod.GET,"/estudiantes").permitAll()
+                .antMatchers(HttpMethod.POST,"/reclutadores").permitAll()
+                .antMatchers(HttpMethod.GET,"/reclutadores").permitAll()
+                .antMatchers(HttpMethod.GET,"/Institucion_Educativa").permitAll()
+                .antMatchers(HttpMethod.GET,"/Institucion_Educativa/{id}").permitAll()
+                .antMatchers(HttpMethod.GET,"/Empresa").permitAll()
+                .antMatchers(HttpMethod.GET,"/Empresa/{id}").permitAll()
+                .antMatchers("/authenticate").permitAll() //.hasAuthority("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and()
+                .formLogin().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
+    }
 }

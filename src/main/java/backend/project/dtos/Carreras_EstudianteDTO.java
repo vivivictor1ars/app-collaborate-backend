6d0747e3646539227930e491eca8cc9df8637e18
ALDAIR2611 <manuelpuma261@gmail.com>
2024-09-27 16:34:56 -0500

    Entity Institucion.java modified

---
+++ src/main/java/backend/project/dtos/Carreras_EstudianteDTO.java
@@ -0,0 +1,37 @@
+package backend.project.dtos;
+
+import backend.project.entities.Carrera;
+import backend.project.entities.Estudiante;
+
+
+public class Carreras_EstudianteDTO {
+    private int id;
+    private Estudiante estudiante;
+    private Carrera carrera;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Estudiante getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(Estudiante estudiante) {
+        this.estudiante = estudiante;
+    }
+
+    public Carrera getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(Carrera carrera) {
+        this.carrera = carrera;
+    }
+
+}
+

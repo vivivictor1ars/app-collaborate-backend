ca868f51c8ee240b3bff1b412cd74c32f75e9edd
ALDAIR2611 <manuelpuma261@gmail.com>
2024-09-27 16:44:04 -0500

    Entity Institucion.java modified

---
+++ src/main/java/backend/project/dtos/HabilidadDTO.java
@@ -0,0 +1,24 @@
+package backend.project.dtos;
+
+public class HabilidadDTO {
+    private int id;
+
+    private String descripcion_Habilidad;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion_Habilidad() {
+        return descripcion_Habilidad;
+    }
+
+    public void setDescripcion_Habilidad(String descripcion_Habilidad) {
+        this.descripcion_Habilidad = descripcion_Habilidad;
+    }
+}

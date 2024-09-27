0830688dde97cce1e3ccf79d0216f92621d91355
Vivi <lolxdd1232@hotmail.com>
2024-09-27 18:54:45 -0500

    Exceptions were deleted

--- src/main/java/backend/project/exceptions/InvalidActionException.java
+++
@@ -1,6 +0,0 @@
-package backend.project.exceptions;
-
-public class InvalidActionException extends RuntimeException{
-
-    public InvalidActionException(String message) { super(message); }
-}
--- src/main/java/backend/project/exceptions/InvalidDataException.java
+++
@@ -1,5 +0,0 @@
-package backend.project.exceptions;
-
-public class InvalidDataException extends RuntimeException{
-    public InvalidDataException( String message) { super(message); }
-}

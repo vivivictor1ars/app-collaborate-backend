794534d10e24e77436fc2ebed0c12db740866ebe
Vivi <lolxdd1232@hotmail.com>
2024-09-27 12:28:56 -0500

    Service ICarreraService.java was modified

--- src/main/java/backend/project/services/EstudianteService.java
+++
@@ -1,4 +0,0 @@
-package backend.project.services;
-
-public interface EstudianteService {
-}
---
+++ src/main/java/backend/project/services/ICarreraService.java
@@ -0,0 +1,14 @@
+package backend.project.services;
+
+import backend.project.entities.Carrera;
+
+import java.util.List;
+
+public interface ICarreraService {
+    public void insert(Carrera carrera);
+    List<Carrera> list();
+
+    public void delete(int id);
+
+    public Carrera ListId(int id);
+}

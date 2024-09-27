f7a80884cd9f098e2b6927f62b9028620fbe0ce7
Vivi <lolxdd1232@hotmail.com>
2024-09-27 11:42:47 -0500

    Controller InstitucionController.java was added

---
+++ src/main/java/backend/project/controllers/InstitucionController.java
@@ -0,0 +1,51 @@
+package backend.project.controllers;
+
+import backend.project.entities.Institucion;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/Institucion_Educativa")
+public class InstitucionController {
+    @Autowired
+    private IInstitucionService iS;
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public void insert(@RequestBody InstitucionDTO dto){
+        ModelMapper m = new ModelMapper();
+        Institucion i = m.map(dto,Institucion.class);
+        iS.insert(i);
+    }
+
+    @GetMapping
+    public List<InstitucionDTO> list(){
+        return iS.list().stream().map(x->{
+            ModelMapper m = new ModelMapper();
+            return m.map(x, InstitucionDTO.class);
+        }).collect(Collectors.toList());
+    }
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public void delete(@PathVariable("id")Integer id){
+        iS.delete(id);
+    }
+    @GetMapping("/{id}")
+    public InstitucionDTO ListId(@PathVariable("id")Integer id){
+        ModelMapper m = new ModelMapper();
+        InstitucionDTO dto = m.map(iS.ListId(id), InstitucionDTO.class);
+        return dto;
+    }
+    @PutMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public void goUpdate(@RequestBody InstitucionDTO dto){
+        ModelMapper m = new ModelMapper();
+        Institucion i = m.map(dto, Institucion.class);
+        iS.insert(i);
+    }
+}

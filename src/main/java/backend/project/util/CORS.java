1ad4690c4e9f7fe03677d3f3be89bdf9380f9a06
Vivi <lolxdd1232@hotmail.com>
2024-09-27 13:53:31 -0500

    Components were added

---
+++ src/main/java/backend/project/util/CORS.java
@@ -0,0 +1,11 @@
+package backend.project.util;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CORS implements Filter {
+
+}

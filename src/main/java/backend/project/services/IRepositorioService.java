79d942396d3a2f9a35faeb933855e6a72e45abe1
ALDAIR2611 <manuelpuma261@gmail.com>
2024-09-27 16:31:31 -0500

    Entity Institucion.java modified

---
+++ src/main/java/backend/project/services/IRepositorioService.java
@@ -0,0 +1,21 @@
+package backend.project.services;
+
+import backend.project.entities.Repositorio;
+import com.upc.collaborateu.dtos.RepositorioEstudianteDTO;
+import com.upc.collaborateu.entities.Repositorio;
+
+import java.util.List;
+
+public interface IRepositorioService {
+    public void insert(Repositorio repositorio);
+
+    List<Repositorio> list();
+
+    public void delete(int id);
+
+    public Repositorio ListId(int id);
+
+    public List<RepositorioEstudianteDTO> reporte02();
+
+}
+

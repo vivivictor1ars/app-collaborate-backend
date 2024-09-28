d90cf87128c1ff9b21553ac7736afa433950373e
JoiceVicente2020 <134071655+JoiceVicente2020@users.noreply.github.com>
2024-09-27 23:15:04 -0500

    Update MatchDTO.java

--- src/main/java/backend/project/dtos/MatchDTO.java
+++ src/main/java/backend/project/dtos/MatchDTO.java
@@ -14,7 +14,7 @@ import lombok.AllArgsConstructor;
 public class MatchDTO {
     private int id;  
     private String codigoMatch; 
-    private boolean confirmacionMatch;  // Confirmación del match
+    private boolean confirmacionMatch;  
     private Reclutador reclutador;  // Reclutador asociado al match
     private Estudiante estudiante;  // Estudiante asociado al match
 }

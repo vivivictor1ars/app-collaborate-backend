5a45cc822d9d78be952498a3bdd08367c7de6f49
Vivi <lolxdd1232@hotmail.com>
2024-10-26 11:16:44 -0500

    New model of the backend is going on

--- src/main/java/backend/project/entities/Carrera.java
+++ src/main/java/backend/project/entities/Carrera.java
@@ -1,5 +1,7 @@
 package backend.project.entities;
 
+import jdk.jfr.DataAmount;
+
 import javax.persistence.*;
 
 
@@ -8,31 +10,6 @@ import javax.persistence.*;
 public class Carrera {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-    @Column(name = "nombre_Carrera", length = 60, nullable = false)//largo de 60 y es obligatorio
-    private  String nombre_Carrera;
-
-    public Carrera(){}
-
-    public Carrera(int id, String nombre_Carrera) {
-        this.id = id;
-        this.nombre_Carrera = nombre_Carrera;
-    }
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre_Carrera() {
-        return nombre_Carrera;
-    }
-
-    public void setNombre_Carrera(String nombre_Carrera) {
-        this.nombre_Carrera = nombre_Carrera;
-    }
-
-
+    private Long id;
+    private String nombre_Carrera;
 }

278b61d45301eaf27495b8a31530a41ba732d4f1
Vivi <lolxdd1232@hotmail.com>
2024-11-04 17:11:57 -0500

    New model of the backend is going on

--- src/main/java/backend/project/repositories/EstudianteRepository.java
+++ src/main/java/backend/project/repositories/EstudianteRepository.java
@@ -9,26 +9,26 @@ import java.util.List;
 
 @Repository
 public interface EstudianteRepository extends JpaRepository<Estudiante, Long> {
-    @Query(value = "select count(e.id_estudiante) as cantidadestudiante, e.semestre_estudiante as Semestre\n" +
-            "FROM estudiantes e\n" +
-            "GROUP BY e.semestre_estudiante;", nativeQuery = true)
-    List<String[]> cantidadestudiantesporsemestre();
+    @Query(value = "select count(e.estudiante_id) as cantidadEstudiante, e.semestre as SemestreEstudiante" +
+            "FROM Estudiantes e" +
+            "GROUP BY e.semestre;", nativeQuery = true)
+    List<String[]> cantidadEstudianteBySemestre();
 
-    @Query(value = "select round(avg(e.edad_estudiante),3) as edadpromedio, count(e.id_estudiante) as totalestudiantes\n" +
-            "from estudiantes e\n;", nativeQuery = true)
-    List<String[]> promedioedad();
+    @Query(value = "select round(avg(e.edad),3) as edadPromedio, count(e.estudiante_id) as totalEstudiantes" +
+            "from Estudiantes e;", nativeQuery = true)
+    List<String[]> promedioEdad();
 
-    @Query(value = "select count(case when e.edad_estudiante > 20 then 1 end) as Estudiantes_mayores,\n" +
-            "  count(case when e.edad_estudiante <= 20 then 1 end) as Estudiantes_menores\n" +
-            "from estudiantes e;", nativeQuery = true)
-    List<String[]> mayoresymenores();
-    @Query(value = "select (count(case when e.practicante_estudiante=true then 1 end)*100/count(*)) as Porcentaje_Busca_Practicas,\n" +
-            "(count(case when e.practicante_estudiante=false then 1 end)*100/count(*)) as Porcentaje_No_Busca_Practicas\n" +
-            "from estudiantes e;", nativeQuery = true)
-    List<String[]> buscapracticasporcentaje();
-    @Query(value = "select count(e.id_estudiante) as CantidadEstudiantes, i.nombre_institucion as Institucion\n" +
-            "from estudiantes e\n" +
-            "join institucion_educativa i on i.id = e.institucion_educativa_id\n" +
-            "group by i.nombre_institucion", nativeQuery = true)
-    List<String[]> estudiantesporinstitucion();
+    @Query(value = "select count(case when e.edad > 20 then 1 end) as EstudiantesMayores" +
+            "  count(case when e.edad <= 20 then 1 end) as EstudiantesMenores" +
+            "from Estudiantes e;", nativeQuery = true)
+    List<String[]> mayoresYmenores();
+    @Query(value = "select (count(case when e.practicante = true then 1 end)*100/count(*)) as PorcentajeBuscaPracticas" +
+            "(count(case when e.practicante = false then 1 end)*100/count(*)) as PorcentajeNoBuscaPracticas\n" +
+            "from Estudiantes e;", nativeQuery = true)
+    List<String[]> buscaPracticasPorcentaje();
+    @Query(value = "select count(e.estudiante_id) as CantidadEstudiantes, i.name as Institucion" +
+            "from Estudiantes e" +
+            "join Institucion_Educativa i on i.id = e.institucion_id" +
+            "group by i.name", nativeQuery = true)
+    List<String[]> estudiantesByInstitucion();
 }

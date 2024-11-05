99dee0fd251c57fbac2c17fafdf15efdb693c10b
Vivi <lolxdd1232@hotmail.com>
2024-11-04 21:15:03 -0500

    New model of the backend is going on

--- src/main/java/backend/project/serviceimpl/Carrera_EstudianteServiceImpl.java
+++ src/main/java/backend/project/serviceimpl/Carrera_EstudianteServiceImpl.java
@@ -5,8 +5,6 @@ import backend.project.services.Carrera_EstudianteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class Carrera_EstudianteServiceImpl implements Carrera_EstudianteService {
     @Autowired
